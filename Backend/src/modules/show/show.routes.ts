1ec1f8c670c0c88d9ef246aedf9c7ca27d9538e5
TANISHQ TIWARI <tanishq.tiwari2812@gmail.com>
2026-05-20 00:00:08 +0530

    show module updated

---
+++ Backend/src/modules/show/show.routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import * as ShowController from "./show.controller";
+
+const router = Router();
+
+router.post("/" , ShowController.createShow);
+router.get("/" , ShowController.getShowsByMovieDateLocation);
+router.get("/:id" , ShowController.getShowById);
+router.put("/:showId" , ShowController.updateSeatsStatus);
+
+export default router;
--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import  express  from "express";
 import movieRouter from "../modules/movie/movie.route"
 import theaterRouter from "../modules/theater/theater.routes";
+import showRouter from "../modules/show/show.routes"
 
 const router = express.Router();
 
 router.use("/movies" , movieRouter);
 router.use("/theaters" , theaterRouter);
+router.use("/shows", showRouter);
 
 export default router;
