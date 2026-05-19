f1b7eab87e49cb50db2d5e894dfc22bd5f448131
TANISHQ TIWARI <tanishq.tiwari2812@gmail.com>
2026-05-19 21:46:41 +0530

    theater module completed

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,7 +1,10 @@
-import express from 'express';
+import  express  from "express";
 import movieRouter from "../modules/movie/movie.route"
+import theaterRouter from "../modules/theater/theater.routes";
+
 const router = express.Router();
 
 router.use("/movies" , movieRouter);
+router.use("/theaters" , theaterRouter);
 
-export default router ;
+export default router;
